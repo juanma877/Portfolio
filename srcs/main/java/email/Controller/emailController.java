adbab72604a2b2de555ac65a3a1c05ce8a71a176
juanma877 <espasandin.j@gmail.com>
2024-06-11 20:28:55 -0300

    eliminar java

--- srcs/main/java/email/Controller/emailController.java
+++
@@ -1,9 +0,0 @@
-@RestController
-public class emailController(){
-    @autowired
-
-    @PostMapping("/clientes/crear")
-    public void crearCliente(@RequestBody Cliente cli) {
-        clienServ.guardarCliente(cli);
-    }
-}
